0441637526480e8bb669fb3019943f9a9a764bae
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2021-09-20 17:47:56 +0600

    js(21-27)

---
+++ javascript tutorial/js/GradeCalculate.js
@@ -0,0 +1,50 @@
+function gradeCalculate(result) {
+
+if(result>=90){
+	document.getElementById('result_ouput').innerHTML="A+";
+	document.getElementById('wish').innerHTML="Congratulations!!";
+	document.getElementById('grade').innerHTML="Grade point: 4.00";
+	
+}
+
+else if(result>=85){
+	document.getElementById('result_ouput').innerHTML="A";
+    document.getElementById('grade').innerHTML="Grade point: 3.75";
+	
+}
+
+else if(result>=80){
+	document.getElementById('result_ouput').innerHTML="B+";
+	document.getElementById('grade').innerHTML="Grade point: 3.50";
+}
+
+else if(result>=75){
+	document.getElementById('result_ouput').innerHTML="B";
+	document.getElementById('grade').innerHTML="Grade point: 3.25";
+}
+
+else if(result>=70){
+	document.getElementById('result_ouput').innerHTML="C+"; 
+	document.getElementById('grade').innerHTML="Grade point: 3.00";
+}
+
+else if(result>=65){
+	document.getElementById('result_ouput').innerHTML="C";
+	document.getElementById('grade').innerHTML="Grade point: 2.75";
+}
+else if(result>=60){
+	document.getElementById('result_ouput').innerHTML="D+";
+	document.getElementById('grade').innerHTML="Grade point: 2.50";
+}
+else if(result>=50){
+	document.getElementById('result_ouput').innerHTML="D";
+	document.getElementById('grade').innerHTML="Grade point: 2.25";
+}
+else{
+	document.getElementById('result_ouput').innerHTML="F";
+	document.getElementById('grade').innerHTML="Grade point: 0.00";
+	
+}
+
+
+}
---
+++ javascript tutorial/js/object.js
@@ -0,0 +1,4 @@
+var university = {department:"4",faculty:"5",Name:"AAU",Address:"Kuratoli"};
+
+document.getElementById('name').innerHTML=university.Name;
+document.getElementById('address').innerHTML=university.Address;
