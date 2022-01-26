6bb714b5ccd8bba35d5c11f0dab2f2fed9f0292e
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-01-26 18:45:20 +0600

    javascriptNew

---
+++ Javascript tutorial New/Js/DomEventForm.js
@@ -0,0 +1,49 @@
+var fname = document.querySelector("input[name=fname]");
+
+fname.addEventListener("change",firstName);
+
+var submit = document.getElementById("submit");
+submit.addEventListener("change",submit);
+var sabmitValue;
+
+    
+
+
+
+
+function firstName(e){
+    var fnameValue = e.target.value;
+
+    document.getElementById("firstName").innerText = fnameValue;
+}
+
+
+var admitquery = document.querySelectorAll("input[name=admit_before]");
+
+Array.from(admitquery).map((admit_before) =>{
+
+    admit_before.addEventListener("change",eventHandler);
+
+
+});
+
+function eventHandler(e){
+    if(e.target.checked){
+        
+        var output = e.target.value;
+        document.getElementById("admitHere").innerText = output;
+    }
+}
+
+
+var courseSelects = document.querySelector("#courseSelect");
+
+courseSelects.addEventListener("change",cousreEventHandler);
+
+function cousreEventHandler(e){
+    document.getElementById("Course").innerText = e.target.value;
+}
+
+
+
+
