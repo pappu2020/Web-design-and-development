3ab775dfe756b8446cb0fe62f76946d56d2af2a3
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-03-17 14:34:23 +0600

    sw

--- Bootstarp NEW/Sample Website 2/js/HomeJs.js
+++ Bootstarp NEW/Sample Website 2/js/HomeJs.js
@@ -3,6 +3,25 @@ var CourseSummary = document.getElementById("CourseSummary");
 var nav2 = document.querySelector(".nav2");
 
 
+
+var chooseImageid = document.getElementById("chooseImageid");
+var chooseParaHeadingid=  document.getElementById("chooseParaHeadingid");
+var chooseParaid=  document.getElementById("chooseParaid");
+
+var chooseImageid2 = document.getElementById("chooseImageid2");
+var chooseParaHeadingid2=  document.getElementById("chooseParaHeadingid2");
+var chooseParaid2=  document.getElementById("chooseParaid2");
+
+var chooseImageid3 = document.getElementById("chooseImageid3");
+var chooseParaHeadingid3=  document.getElementById("chooseParaHeadingid3");
+var chooseParaid3=  document.getElementById("chooseParaid3");
+
+var chooseImageid4 = document.getElementById("chooseImageid4");
+var chooseParaHeadingid4=  document.getElementById("chooseParaHeadingid4");
+var chooseParaid4=  document.getElementById("chooseParaid4");
+
+
+
 courses.addEventListener("mouseenter",function(){
     CourseSummary.style.display="block";
     nav2.style.color="#6366F1";
@@ -15,6 +34,115 @@ CourseSummary.addEventListener("mouseleave",function(){
     nav2.style.color="white";
     
 })
+
+
+chooseImageid.addEventListener("mouseover",function(){
+    chooseParaHeadingid.style.color="#0DCAF0";
+    chooseParaid.style.color="#0DCAF0";
+    chooseImageid.style.border="5px solid #0DCAF0";
+})
+
+chooseImageid.addEventListener("mouseleave",function(){
+    chooseParaHeadingid.style.color="white";
+    chooseParaid.style.color="white";
+    chooseImageid.style.border="none";
+})
+
+
+chooseImageid2.addEventListener("mouseover",function(){
+    chooseParaHeadingid2.style.color="#0DCAF0";
+    chooseParaid2.style.color="#0DCAF0";
+    chooseImageid2.style.border="5px solid #0DCAF0";
+})
+
+chooseImageid2.addEventListener("mouseleave",function(){
+    chooseParaHeadingid2.style.color="white";
+    chooseParaid2.style.color="white";
+    chooseImageid2.style.border="none";
+})
+
+chooseImageid3.addEventListener("mouseover",function(){
+    chooseParaHeadingid3.style.color="#0DCAF0";
+    chooseParaid3.style.color="#0DCAF0";
+    chooseImageid3.style.border="5px solid #0DCAF0";
+})
+
+chooseImageid3.addEventListener("mouseleave",function(){
+    chooseParaHeadingid3.style.color="white";
+    chooseParaid3.style.color="white";
+    chooseImageid3.style.border="none";
+})
+
+chooseImageid4.addEventListener("mouseover",function(){
+    chooseParaHeadingid4.style.color="#0DCAF0";
+    chooseParaid4.style.color="#0DCAF0";
+    chooseImageid4.style.border="5px solid #0DCAF0";
+})
+
+chooseImageid4.addEventListener("mouseleave",function(){
+    chooseParaHeadingid4.style.color="white";
+    chooseParaid4.style.color="white";
+    chooseImageid4.style.border="none";
+})
+
+
+
+
+
+
+
+chooseParaHeadingid.addEventListener("mouseover",function(){
+    chooseParaHeadingid.style.color="#0DCAF0";
+    chooseParaid.style.color="#0DCAF0";
+    chooseImageid.style.border="5px solid #0DCAF0";
+})
+
+chooseParaHeadingid.addEventListener("mouseleave",function(){
+    chooseParaHeadingid.style.color="white";
+    chooseParaid.style.color="white";
+    chooseImageid.style.border="none";
+})
+
+
+chooseParaHeadingid2.addEventListener("mouseover",function(){
+    chooseParaHeadingid2.style.color="#0DCAF0";
+    chooseParaid2.style.color="#0DCAF0";
+    chooseImageid2.style.border="5px solid #0DCAF0";
+})
+
+chooseParaHeadingid2.addEventListener("mouseleave",function(){
+    chooseParaHeadingid2.style.color="white";
+    chooseParaid2.style.color="white";
+    chooseImageid2.style.border="none";
+})
+
+chooseParaHeadingid3.addEventListener("mouseover",function(){
+    chooseParaHeadingid3.style.color="#0DCAF0";
+    chooseParaid3.style.color="#0DCAF0";
+    chooseImageid3.style.border="5px solid #0DCAF0";
+})
+
+chooseParaHeadingid3.addEventListener("mouseleave",function(){
+    chooseParaHeadingid3.style.color="white";
+    chooseParaid3.style.color="white";
+    chooseImageid3.style.border="none";
+})
+
+chooseParaHeadingid4.addEventListener("mouseover",function(){
+    chooseParaHeadingid4.style.color="#0DCAF0";
+    chooseParaid4.style.color="#0DCAF0";
+    chooseImageid4.style.border="5px solid #0DCAF0";
+})
+
+chooseParaHeadingid4.addEventListener("mouseleave",function(){
+    chooseParaHeadingid4.style.color="white";
+    chooseParaid4.style.color="white";
+    chooseImageid4.style.border="none";
+})
+
+
+
+
 
 
 
