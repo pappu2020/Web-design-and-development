aa3c62bbd7763b55a9e67ebdbc29f999420ecc31
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-03-22 20:27:11 +0600

    sw2

--- Bootstarp NEW/Sample Website 2/js/HomeJs.js
+++ Bootstarp NEW/Sample Website 2/js/HomeJs.js
@@ -5,139 +5,139 @@ var nav2 = document.querySelector(".nav2");
 
 
 var chooseImageid = document.getElementById("chooseImageid");
-var chooseParaHeadingid=  document.getElementById("chooseParaHeadingid");
-var chooseParaid=  document.getElementById("chooseParaid");
+var chooseParaHeadingid = document.getElementById("chooseParaHeadingid");
+var chooseParaid = document.getElementById("chooseParaid");
 
 var chooseImageid2 = document.getElementById("chooseImageid2");
-var chooseParaHeadingid2=  document.getElementById("chooseParaHeadingid2");
-var chooseParaid2=  document.getElementById("chooseParaid2");
+var chooseParaHeadingid2 = document.getElementById("chooseParaHeadingid2");
+var chooseParaid2 = document.getElementById("chooseParaid2");
 
 var chooseImageid3 = document.getElementById("chooseImageid3");
-var chooseParaHeadingid3=  document.getElementById("chooseParaHeadingid3");
-var chooseParaid3=  document.getElementById("chooseParaid3");
+var chooseParaHeadingid3 = document.getElementById("chooseParaHeadingid3");
+var chooseParaid3 = document.getElementById("chooseParaid3");
 
 var chooseImageid4 = document.getElementById("chooseImageid4");
-var chooseParaHeadingid4=  document.getElementById("chooseParaHeadingid4");
-var chooseParaid4=  document.getElementById("chooseParaid4");
+var chooseParaHeadingid4 = document.getElementById("chooseParaHeadingid4");
+var chooseParaid4 = document.getElementById("chooseParaid4");
 
 
 
-courses.addEventListener("mouseenter",function(){
-    CourseSummary.style.display="block";
-    nav2.style.color="#6366F1";
+courses.addEventListener("mouseenter", function () {
+    CourseSummary.style.display = "block";
+    nav2.style.color = "#6366F1";
 
-   
+
 })
 
-CourseSummary.addEventListener("mouseleave",function(){
-    CourseSummary.style.display="none";
-    nav2.style.color="white";
-    
-})
+CourseSummary.addEventListener("mouseleave", function () {
+    CourseSummary.style.display = "none";
+    nav2.style.color = "white";
 
-
-chooseImageid.addEventListener("mouseover",function(){
-    chooseParaHeadingid.style.color="#0DCAF0";
-    chooseParaid.style.color="#0DCAF0";
-    chooseImageid.style.border="5px solid #0DCAF0";
-})
-
-chooseImageid.addEventListener("mouseleave",function(){
-    chooseParaHeadingid.style.color="white";
-    chooseParaid.style.color="white";
-    chooseImageid.style.border="none";
 })
 
 
-chooseImageid2.addEventListener("mouseover",function(){
-    chooseParaHeadingid2.style.color="#0DCAF0";
-    chooseParaid2.style.color="#0DCAF0";
-    chooseImageid2.style.border="5px solid #0DCAF0";
+chooseImageid.addEventListener("mouseover", function () {
+    chooseParaHeadingid.style.color = "#0DCAF0";
+    chooseParaid.style.color = "#0DCAF0";
+    chooseImageid.style.border = "5px solid #0DCAF0";
 })
 
-chooseImageid2.addEventListener("mouseleave",function(){
-    chooseParaHeadingid2.style.color="white";
-    chooseParaid2.style.color="white";
-    chooseImageid2.style.border="none";
-})
-
-chooseImageid3.addEventListener("mouseover",function(){
-    chooseParaHeadingid3.style.color="#0DCAF0";
-    chooseParaid3.style.color="#0DCAF0";
-    chooseImageid3.style.border="5px solid #0DCAF0";
-})
-
-chooseImageid3.addEventListener("mouseleave",function(){
-    chooseParaHeadingid3.style.color="white";
-    chooseParaid3.style.color="white";
-    chooseImageid3.style.border="none";
-})
-
-chooseImageid4.addEventListener("mouseover",function(){
-    chooseParaHeadingid4.style.color="#0DCAF0";
-    chooseParaid4.style.color="#0DCAF0";
-    chooseImageid4.style.border="5px solid #0DCAF0";
-})
-
-chooseImageid4.addEventListener("mouseleave",function(){
-    chooseParaHeadingid4.style.color="white";
-    chooseParaid4.style.color="white";
-    chooseImageid4.style.border="none";
+chooseImageid.addEventListener("mouseleave", function () {
+    chooseParaHeadingid.style.color = "white";
+    chooseParaid.style.color = "white";
+    chooseImageid.style.border = "none";
 })
 
 
-
-
-
-
-
-chooseParaHeadingid.addEventListener("mouseover",function(){
-    chooseParaHeadingid.style.color="#0DCAF0";
-    chooseParaid.style.color="#0DCAF0";
-    chooseImageid.style.border="5px solid #0DCAF0";
+chooseImageid2.addEventListener("mouseover", function () {
+    chooseParaHeadingid2.style.color = "#0DCAF0";
+    chooseParaid2.style.color = "#0DCAF0";
+    chooseImageid2.style.border = "5px solid #0DCAF0";
 })
 
-chooseParaHeadingid.addEventListener("mouseleave",function(){
-    chooseParaHeadingid.style.color="white";
-    chooseParaid.style.color="white";
-    chooseImageid.style.border="none";
+chooseImageid2.addEventListener("mouseleave", function () {
+    chooseParaHeadingid2.style.color = "white";
+    chooseParaid2.style.color = "white";
+    chooseImageid2.style.border = "none";
+})
+
+chooseImageid3.addEventListener("mouseover", function () {
+    chooseParaHeadingid3.style.color = "#0DCAF0";
+    chooseParaid3.style.color = "#0DCAF0";
+    chooseImageid3.style.border = "5px solid #0DCAF0";
+})
+
+chooseImageid3.addEventListener("mouseleave", function () {
+    chooseParaHeadingid3.style.color = "white";
+    chooseParaid3.style.color = "white";
+    chooseImageid3.style.border = "none";
+})
+
+chooseImageid4.addEventListener("mouseover", function () {
+    chooseParaHeadingid4.style.color = "#0DCAF0";
+    chooseParaid4.style.color = "#0DCAF0";
+    chooseImageid4.style.border = "5px solid #0DCAF0";
+})
+
+chooseImageid4.addEventListener("mouseleave", function () {
+    chooseParaHeadingid4.style.color = "white";
+    chooseParaid4.style.color = "white";
+    chooseImageid4.style.border = "none";
 })
 
 
-chooseParaHeadingid2.addEventListener("mouseover",function(){
-    chooseParaHeadingid2.style.color="#0DCAF0";
-    chooseParaid2.style.color="#0DCAF0";
-    chooseImageid2.style.border="5px solid #0DCAF0";
+
+
+
+
+
+chooseParaHeadingid.addEventListener("mouseover", function () {
+    chooseParaHeadingid.style.color = "#0DCAF0";
+    chooseParaid.style.color = "#0DCAF0";
+    chooseImageid.style.border = "5px solid #0DCAF0";
 })
 
-chooseParaHeadingid2.addEventListener("mouseleave",function(){
-    chooseParaHeadingid2.style.color="white";
-    chooseParaid2.style.color="white";
-    chooseImageid2.style.border="none";
+chooseParaHeadingid.addEventListener("mouseleave", function () {
+    chooseParaHeadingid.style.color = "white";
+    chooseParaid.style.color = "white";
+    chooseImageid.style.border = "none";
 })
 
-chooseParaHeadingid3.addEventListener("mouseover",function(){
-    chooseParaHeadingid3.style.color="#0DCAF0";
-    chooseParaid3.style.color="#0DCAF0";
-    chooseImageid3.style.border="5px solid #0DCAF0";
+
+chooseParaHeadingid2.addEventListener("mouseover", function () {
+    chooseParaHeadingid2.style.color = "#0DCAF0";
+    chooseParaid2.style.color = "#0DCAF0";
+    chooseImageid2.style.border = "5px solid #0DCAF0";
 })
 
-chooseParaHeadingid3.addEventListener("mouseleave",function(){
-    chooseParaHeadingid3.style.color="white";
-    chooseParaid3.style.color="white";
-    chooseImageid3.style.border="none";
+chooseParaHeadingid2.addEventListener("mouseleave", function () {
+    chooseParaHeadingid2.style.color = "white";
+    chooseParaid2.style.color = "white";
+    chooseImageid2.style.border = "none";
 })
 
-chooseParaHeadingid4.addEventListener("mouseover",function(){
-    chooseParaHeadingid4.style.color="#0DCAF0";
-    chooseParaid4.style.color="#0DCAF0";
-    chooseImageid4.style.border="5px solid #0DCAF0";
+chooseParaHeadingid3.addEventListener("mouseover", function () {
+    chooseParaHeadingid3.style.color = "#0DCAF0";
+    chooseParaid3.style.color = "#0DCAF0";
+    chooseImageid3.style.border = "5px solid #0DCAF0";
 })
 
-chooseParaHeadingid4.addEventListener("mouseleave",function(){
-    chooseParaHeadingid4.style.color="white";
-    chooseParaid4.style.color="white";
-    chooseImageid4.style.border="none";
+chooseParaHeadingid3.addEventListener("mouseleave", function () {
+    chooseParaHeadingid3.style.color = "white";
+    chooseParaid3.style.color = "white";
+    chooseImageid3.style.border = "none";
+})
+
+chooseParaHeadingid4.addEventListener("mouseover", function () {
+    chooseParaHeadingid4.style.color = "#0DCAF0";
+    chooseParaid4.style.color = "#0DCAF0";
+    chooseImageid4.style.border = "5px solid #0DCAF0";
+})
+
+chooseParaHeadingid4.addEventListener("mouseleave", function () {
+    chooseParaHeadingid4.style.color = "white";
+    chooseParaid4.style.color = "white";
+    chooseImageid4.style.border = "none";
 })
 
 
@@ -163,34 +163,34 @@ var passwordCheck = document.getElementById("passwordCheck");
 //     message.style.marginBottom = "15px";
 //     message.style.border = "3px solid black";
 //     message.style.position = "absolute";
-    
+
 
 // })
 
 
-Password.addEventListener("focus",function(){
+Password.addEventListener("focus", function () {
     message.style.display = "block";
     message.style.marginTop = "-140px";
-    
+
     message.style.border = "3px solid black";
     message.style.position = "absolute";
     message.style.marginLeft = "300px";
-    
+
 
 })
 
-Password.addEventListener("blur",function(){
+Password.addEventListener("blur", function () {
     message.style.display = "none";
 })
 
-Password.addEventListener("keyup",function(){
+Password.addEventListener("keyup", function () {
     var lowerCaseLetters = /[a-z]/g;
-    if(Password.value.match(lowerCaseLetters)){
+    if (Password.value.match(lowerCaseLetters)) {
         lower.classList.add("valid");
         lower.classList.remove("invalid");
     }
 
-    else{
+    else {
         lower.classList.remove("valid");
         lower.classList.add("invalid");
     }
@@ -198,83 +198,130 @@ Password.addEventListener("keyup",function(){
 
 
     var upperCaseLetters = /[A-Z]/g;
-    if(Password.value.match(upperCaseLetters)){
+    if (Password.value.match(upperCaseLetters)) {
         upper.classList.add("valid");
         upper.classList.remove("invalid");
     }
 
-    else{
+    else {
         upper.classList.remove("valid");
         upper.classList.add("invalid");
     }
 
 
     var numbers = /[0-9]/g;
-    if(Password.value.match(numbers)){
+    if (Password.value.match(numbers)) {
         num.classList.add("valid");
         num.classList.remove("invalid");
     }
 
-    else{
+    else {
         num.classList.remove("valid");
         num.classList.add("invalid");
     }
 
-    if(Password.value.length >= 8) {
+    if (Password.value.length >= 8) {
         min.classList.remove("invalid");
         min.classList.add("valid");
-      } else {
+    } else {
         min.classList.remove("valid");
         min.classList.add("invalid");
-      }
+    }
 })
 
 
-function showPassword(){
-    if(Password.type == "password"){
-        Password.type="text";
+function showPassword() {
+    if (Password.type == "password") {
+        Password.type = "text";
     }
 
-    else{
-        Password.type="password";
-    }
-}
-
-function showConPassword(){
-    if(Confirm_pass.type == "password"){
-        Confirm_pass.type="text";
-    }
-
-    else{
-        Confirm_pass.type="password";
+    else {
+        Password.type = "password";
     }
 }
 
+function showConPassword() {
+    if (Confirm_pass.type == "password") {
+        Confirm_pass.type = "text";
+    }
+
+    else {
+        Confirm_pass.type = "password";
+    }
+}
 
 
-form.addEventListener("submit",eventHandler);
 
-   
-   function eventHandler(e){
-   e.preventDefault();
-
-   if(Confirm_pass.value != Password.value){
-    document.getElementById("passwordCheck").innerText = "Password do not match";
-    return false;
-   }
-
-   else{
-       confirm("Your registration complete!!!")
-   }
-
-   if(true){
-    window.location.assign("./Home.html");
-   }
+form.addEventListener("submit", eventHandler);
 
 
-  
+function eventHandler(e) {
+    e.preventDefault();
 
-   }
+    if (Confirm_pass.value != Password.value) {
+        document.getElementById("passwordCheck").innerText = "Password do not match";
+        return false;
+    }
+
+    else {
+        confirm("Your registration complete!!!")
+    }
+
+    if (true) {
+        window.location.assign("./Home.html");
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.textAnimation1 .letters1');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.textAnimation1 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.textAnimation1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+  // Wrap every letter in a span
+var textWrapper2 = document.querySelector('.textAnimation2 .letters2');
+textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter2'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.textAnimation2 .letter2',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.textAnimation2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
 
 
 
