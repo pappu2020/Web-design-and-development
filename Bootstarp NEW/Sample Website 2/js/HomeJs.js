b421c81e5ac083e67e457478670293107b5869f5
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-03-24 00:06:27 +0600

    sw2

--- Bootstarp NEW/Sample Website 2/js/HomeJs.js
+++ Bootstarp NEW/Sample Website 2/js/HomeJs.js
@@ -1,6 +1,20 @@
+var preloader = document.getElementById("preloader");
+
+window.addEventListener("load",function(){
+    setTimeout(()=>{
+    preloader.style.display="none";
+    },2500);
+    
+    
+}) 
+
+
+
 var courses = document.getElementById("courses");
 var CourseSummary = document.getElementById("CourseSummary");
 var nav2 = document.querySelector(".nav2");
+var nav5 = document.querySelector(".nav5");
+
 
 
 
@@ -20,14 +34,49 @@ var chooseImageid4 = document.getElementById("chooseImageid4");
 var chooseParaHeadingid4 = document.getElementById("chooseParaHeadingid4");
 var chooseParaid4 = document.getElementById("chooseParaid4");
 
+var branches = document.getElementById("branches");
+var branchesId= document.getElementById("branchesId");
+
+
+
+
+branches.addEventListener("mouseenter",function(){
+    branchesId.style.display = "block";
+    branchesId.style.marginLeft = "736px";
+    branchesId.style.marginTop = "55px";
+    CourseSummary.style.display = "none";
+    nav2.style.color = "white";
+    nav5.style.color = "#6366F1";
+
+
+   
+
+})
+
+
+branchesId.addEventListener("mouseleave",function(){
+    branchesId.style.display = "none";
+    nav5.style.color = "white";
+   
+})
+
+
+
+
+
+
 
 
 courses.addEventListener("mouseenter", function () {
     CourseSummary.style.display = "block";
     nav2.style.color = "#6366F1";
+    branchesId.style.display = "none";
+    nav5.style.color = "white";
 
 
 })
+
+
 
 CourseSummary.addEventListener("mouseleave", function () {
     CourseSummary.style.display = "none";
