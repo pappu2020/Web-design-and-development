f01d6f41a632d5608e0a8bcc9263411b0961f40a
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-03-29 23:57:34 +0600

    pappuWebsite

---
+++ Pappu Saha Website/js/JSfile.js
@@ -0,0 +1,9 @@
+var SectionRightContentHeading = document.querySelector(".SectionRightContentHeading");
+
+
+
+window.addEventListener("load",function(){
+    SectionRightContentHeading.classList.add("SectionRightContentHeadingAnim");
+    // SectionRightContentParaAnim.classList.add("SectionRightContentParaAnim");
+ 
+ })
