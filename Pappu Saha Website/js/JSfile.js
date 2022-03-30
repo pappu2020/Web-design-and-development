8392cf44044217798acdcc34d6ba04302adb1d53
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-03-31 01:09:31 +0600

    pappuWebsite

--- Pappu Saha Website/js/JSfile.js
+++ Pappu Saha Website/js/JSfile.js
@@ -1,6 +1,6 @@
 var homeButton = document.querySelector(".homeButton");
 var aboutButton = document.querySelector(".aboutButton");
-
+var serviceButton = document.querySelector(".serviceButton"); 
 
 
 
@@ -9,12 +9,22 @@ homeButton.addEventListener("click",function(){
     window.location.assign("#HomeId");
     homeButton.style.color = "blue";
     aboutButton.style.color = "#C7C8C9";
+    serviceButton.style.color = "#C7C8C9";
 })
 
 aboutButton.addEventListener("click",function(){
     window.location.assign("#aboutSection");
     homeButton.style.color = "#C7C8C9";
     aboutButton.style.color = "blue";
+    serviceButton.style.color = "#C7C8C9";
+})
+
+
+serviceButton.addEventListener("click",function(){
+    window.location.assign("#serviceSection");
+    homeButton.style.color = "#C7C8C9";
+    aboutButton.style.color = "#C7C8C9";
+    serviceButton.style.color = "blue";
 })
 
 
@@ -33,3 +43,183 @@ window.addEventListener("load",function(){
     // SectionRightContentParaAnim.classList.add("SectionRightContentParaAnim");
  
  })
+
+
+
+
+ var stage1 = document.querySelector(".stage1");
+ var stage2 = document.querySelector(".stage2");
+ var stage3 = document.querySelector(".stage3");
+ var stage4 = document.querySelector(".stage4");
+ var stage5 = document.querySelector(".stage5");
+ var stage6 = document.querySelector(".stage6");
+ var stage7 = document.querySelector(".stage7");
+
+ var stage1Para = document.querySelector(".stage1Para");
+ var stage2Para = document.querySelector(".stage2Para");
+ var stage3Para = document.querySelector(".stage3Para");
+ var stage4Para = document.querySelector(".stage4Para");
+ var stage5Para = document.querySelector(".stage5Para");
+ var stage6Para = document.querySelector(".stage6Para");
+ var stage7Para = document.querySelector(".stage7Para");
+
+
+ stage1.addEventListener("mouseover",function(){
+     stage1.style.border = "3px solid #0DCAF0"
+     stage1Para.style.color = "#0DCAF0";
+ })
+
+ stage1.addEventListener("mouseleave",function(){
+    stage1.style.border = "none";
+    stage1Para.style.color = "black";
+})
+
+
+stage1Para.addEventListener("mouseover",function(){
+    stage1.style.border = "3px solid #0DCAF0"
+    stage1Para.style.color = "#0DCAF0";
+})
+
+stage1Para.addEventListener("mouseleave",function(){
+   stage1.style.border = "none";
+   stage1Para.style.color = "black";
+})
+
+
+
+
+stage2.addEventListener("mouseover",function(){
+    stage2.style.border = "3px solid #0DCAF0"
+    stage2Para.style.color = "#0DCAF0";
+})
+
+stage2.addEventListener("mouseleave",function(){
+   stage2.style.border = "none";
+   stage2Para.style.color = "black";
+})
+
+
+stage2Para.addEventListener("mouseover",function(){
+   stage2.style.border = "3px solid #0DCAF0"
+   stage2Para.style.color = "#0DCAF0";
+})
+
+stage2Para.addEventListener("mouseleave",function(){
+  stage2.style.border = "none";
+  stage2Para.style.color = "black";
+})
+
+
+
+stage3.addEventListener("mouseover",function(){
+    stage3.style.border = "3px solid #0DCAF0"
+    stage3Para.style.color = "#0DCAF0";
+})
+
+stage3.addEventListener("mouseleave",function(){
+    stage3.style.border = "none";
+    stage3Para.style.color = "black";
+})
+
+
+stage3Para.addEventListener("mouseover",function(){
+    stage3.style.border = "3px solid #0DCAF0"
+    stage3Para.style.color = "#0DCAF0";
+})
+
+stage3Para.addEventListener("mouseleave",function(){
+    stage3.style.border = "none";
+    stage3Para.style.color = "black";
+})
+
+
+
+
+stage4.addEventListener("mouseover",function(){
+    stage4.style.border = "3px solid #0DCAF0"
+    stage4Para.style.color = "#0DCAF0";
+})
+
+stage4.addEventListener("mouseleave",function(){
+    stage4.style.border = "none";
+    stage4Para.style.color = "black";
+})
+
+
+stage4Para.addEventListener("mouseover",function(){
+    stage4.style.border = "3px solid #0DCAF0"
+    stage4Para.style.color = "#0DCAF0";
+})
+
+stage4Para.addEventListener("mouseleave",function(){
+    stage4.style.border = "none";
+    stage4Para.style.color = "black";
+})
+
+
+
+stage5.addEventListener("mouseover",function(){
+    stage5.style.border = "3px solid #0DCAF0"
+    stage5Para.style.color = "#0DCAF0";
+})
+
+stage5.addEventListener("mouseleave",function(){
+    stage5.style.border = "none";
+    stage5Para.style.color = "black";
+})
+
+
+stage5Para.addEventListener("mouseover",function(){
+    stage5.style.border = "3px solid #0DCAF0"
+    stage5Para.style.color = "#0DCAF0";
+})
+
+stage5Para.addEventListener("mouseleave",function(){
+    stage5.style.border = "none";
+    stage5Para.style.color = "black";
+})
+
+
+stage6.addEventListener("mouseover",function(){
+    stage6.style.border = "3px solid #0DCAF0"
+    stage6Para.style.color = "#0DCAF0";
+})
+
+stage6.addEventListener("mouseleave",function(){
+    stage6.style.border = "none";
+    stage6Para.style.color = "black";
+})
+
+
+stage6Para.addEventListener("mouseover",function(){
+    stage6.style.border = "3px solid #0DCAF0"
+    stage6Para.style.color = "#0DCAF0";
+})
+
+stage6Para.addEventListener("mouseleave",function(){
+    stage6.style.border = "none";
+    stage6Para.style.color = "black";
+})
+
+
+
+stage7.addEventListener("mouseover",function(){
+    stage7.style.border = "3px solid #0DCAF0"
+    stage7Para.style.color = "#0DCAF0";
+})
+
+stage7.addEventListener("mouseleave",function(){
+    stage7.style.border = "none";
+    stage7Para.style.color = "black";
+})
+
+
+stage7Para.addEventListener("mouseover",function(){
+    stage7.style.border = "3px solid #0DCAF0"
+    stage7Para.style.color = "#0DCAF0";
+})
+
+stage7Para.addEventListener("mouseleave",function(){
+    stage7.style.border = "none";
+    stage7Para.style.color = "black";
+})
