105c0e4e57132a49b1c113a89f133ca0aaa4315a
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-03-30 14:15:18 +0600

    pappuWebsite

--- Pappu Saha Website/js/JSfile.js
+++ Pappu Saha Website/js/JSfile.js
@@ -1,7 +1,33 @@
+var homeButton = document.querySelector(".homeButton");
+var aboutButton = document.querySelector(".aboutButton");
+
+
+
+
+
+homeButton.addEventListener("click",function(){
+    window.location.assign("#HomeId");
+    homeButton.style.color = "blue";
+    aboutButton.style.color = "#C7C8C9";
+})
+
+aboutButton.addEventListener("click",function(){
+    window.location.assign("#aboutSection");
+    homeButton.style.color = "#C7C8C9";
+    aboutButton.style.color = "blue";
+})
+
+
+
+
+
+
+
+
+
+
+
 var SectionRightContentHeading = document.querySelector(".SectionRightContentHeading");
-
-
-
 window.addEventListener("load",function(){
     SectionRightContentHeading.classList.add("SectionRightContentHeadingAnim");
     // SectionRightContentParaAnim.classList.add("SectionRightContentParaAnim");
