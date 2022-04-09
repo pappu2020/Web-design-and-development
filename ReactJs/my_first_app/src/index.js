03738c597c3e4fc2d42453feb7e3dd4dc6ee8987
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-04-09 14:10:36 +0600

    pappuWebsite&ReactJs

--- ReactJs/my_first_app/src/index.js
+++ ReactJs/my_first_app/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var name= "Pappu saha";
+var id = 18;
+var department = "cse";
+
+
+// jsx expression
+
 
 ReactDOM.render(
-  "Hello Pappu",
+  <div>
+    <p>Name: {name}</p>
+    <p>id: {id}</p>
+    <p>Department: {department}</p>
+    <h1>{new Date().getDate()}</h1>
+  </div>,
   document.getElementById('root')
 );
 
