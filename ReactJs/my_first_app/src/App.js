bb1eb6d1c38c98415ad9d66431d4287360b5840c
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-04-28 22:04:47 +0600

    pappuSahaWebsite2

--- ReactJs/my_first_app/src/App.js
+++ ReactJs/my_first_app/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "./components/MyDesign";
+import Data from "./data.json";
 
+
+// mapping data to the components
 function APP(){
     return <div>
-     <Card />,
-     {/* <Intro /> */}
+     <Card name={Data[0].name} id={Data[0].ID} address={Data[0].Address}/>,
+    
     </div>
 }
+
 
 export default APP;
