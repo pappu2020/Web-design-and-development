5d7071c46fac84f2c17074d362512a118bb35fc5
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-04-30 23:58:37 +0600

    pappuWebsite2

--- pappu saha website 2/js/index.js
+++ pappu saha website 2/js/index.js
@@ -12,3 +12,78 @@ window.addEventListener("scroll",()=>{
 })
 
 
+
+// var myCardDiv = document.getElementById("myCardDiv");
+
+// myCardDiv.addEventListener("mouseenter",function(){
+//     myCardDiv.classList.add("serviceSectionAnimClass");
+    
+// })
+
+// myCardDiv.addEventListener("mouseleave",function(){
+//     myCardDiv.classList.remove("serviceSectionAnimClass");
+// })
+
+var myCardDivid1 = document.querySelector(".myCardDivid1");
+var myCardDivid2 = document.querySelector(".myCardDivid2");
+var myCardDivid3 = document.querySelector(".myCardDivid3");
+var myCardDivid4 = document.querySelector(".myCardDivid4");
+var myCardDivid5 = document.querySelector(".myCardDivid5");
+var myCardDivid6 = document.querySelector(".myCardDivid6");
+
+
+myCardDivid1.addEventListener("mouseenter",function(){
+    myCardDivid1.classList.add("serviceSectionAnimClass");
+    
+})
+
+myCardDivid1.addEventListener("mouseleave",function(){
+    myCardDivid1.classList.remove("serviceSectionAnimClass");
+})
+
+
+myCardDivid2.addEventListener("mouseenter",function(){
+    myCardDivid2.classList.add("serviceSectionAnimClass");
+    
+})
+
+myCardDivid2.addEventListener("mouseleave",function(){
+    myCardDivid2.classList.remove("serviceSectionAnimClass");
+})
+
+myCardDivid3.addEventListener("mouseenter",function(){
+    myCardDivid3.classList.add("serviceSectionAnimClass");
+    
+})
+
+myCardDivid3.addEventListener("mouseleave",function(){
+    myCardDivid3.classList.remove("serviceSectionAnimClass");
+})
+
+
+myCardDivid4.addEventListener("mouseenter",function(){
+    myCardDivid4.classList.add("serviceSectionAnimClass");
+    
+})
+
+myCardDivid4.addEventListener("mouseleave",function(){
+    myCardDivid4.classList.remove("serviceSectionAnimClass");
+})
+
+myCardDivid5.addEventListener("mouseenter",function(){
+    myCardDivid5.classList.add("serviceSectionAnimClass");
+    
+})
+
+myCardDivid5.addEventListener("mouseleave",function(){
+    myCardDivid5.classList.remove("serviceSectionAnimClass");
+})
+
+myCardDivid6.addEventListener("mouseenter",function(){
+    myCardDivid6.classList.add("serviceSectionAnimClass");
+    
+})
+
+myCardDivid6.addEventListener("mouseleave",function(){
+    myCardDivid6.classList.remove("serviceSectionAnimClass");
+})
