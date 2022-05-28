6221882979c177b5d43c30a726d6eaf56b2d2aff
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-05-28 23:09:39 +0600

    pappusahaWebsite3

---
+++ Java/forLoop.java
@@ -0,0 +1,11 @@
+public class forLoop {
+   public static void main(String[] args) {
+    int sum = 0;
+       for(int i=0; i<=100; i++){
+        
+           sum = sum+i;
+       }
+
+      System.out.println(sum);
+   }
+}
--- Java/javaArray.java
+++ Java/javaArray.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
 public class javaArray {
     public static void main(String[] args){
-        String[] country={"Bangladesh","India","pakistan"};
+        // String[] country={"Bangladesh","India","pakistan"};
 
-        for(int x=0; x<country.length; x++){
-            System.out.println(country[x]);
+        // for(int x=0; x<country.length; x++){
+        //     System.out.println(country[x]);
+        // }
+
+
+
+        float[] mySum =new float[10];
+
+        float sum = 0;
+
+        
+
+        System.out.println("Enter the Number: ");
+
+        for(int x=0; x<=mySum.length; x++){
+         Scanner myInput = new Scanner(System.in);
+         float input= myInput.nextFloat();
+         mySum[x] = input;
+            sum = sum+mySum[x];
+
+
+            if(mySum[x] == 0){
+                 break;
+            }
         }
-    }  
+
+        System.out.print("Your input value are: " + mySum);
+        System.out.print("Result: " + sum);
+
+
+
+
+        
+}
 }
