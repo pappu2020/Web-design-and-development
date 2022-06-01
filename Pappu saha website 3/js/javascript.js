76a2921d345c4fd31662c0836e9e1f185934094f
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-06-02 00:40:53 +0600

    pappuSahaWebsite3

--- Pappu saha website 3/js/javascript.js
+++ Pappu saha website 3/js/javascript.js
@@ -237,3 +237,25 @@ window.addEventListener("scroll", () => {
 })
 
 
+
+
+
+
+//skills section
+
+let number = document.getElementById("number");
+let count = 0;
+
+setInterval(() => {
+    if (count == 65) {
+        clearInterval();
+    }
+
+    else {
+        count += 1;
+        number.innerHTML = count + "%";
+    }
+
+}, 30);
+
+
